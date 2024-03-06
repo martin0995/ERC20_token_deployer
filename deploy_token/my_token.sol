e5348bc20955ae77a9f390107334ad16526b279a
martinh <mhofmann@senseinode.com>
2024-03-06 14:31:07 -0600

    Updated function with general names

--- deploy_token/my_token.sol
+++ deploy_token/my_token.sol
@@ -4,9 +4,9 @@ pragma solidity 0.8.20;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract HofmannToken is ERC20, Ownable {
+contract TestToken is ERC20, Ownable {
 
-    constructor(address initialOwner) ERC20("Hofmann", "BIKE") Ownable(initialOwner) {}
+    constructor(address initialOwner) ERC20("token_name", "token_symbol") Ownable(initialOwner) {}
 
     function mintFifty() public onlyOwner {
         _mint(msg.sender, 50 * 10**18);
